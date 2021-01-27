0277b05eefef6af345d5490a74e774ad2134005f
cemanaral <cemanaral425@gmail.com>
2021-01-28 02:11:29 +0300

    added selenium functionality

--- bys.py
+++ bys.py
@@ -1,11 +1,18 @@
-import sys
+import sys, os
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+BYS_LOGIN = "https://bys.marmara.edu.tr/v2/Account/Login"
+
 def main(username, password):
     print("username: " + username)
     print("password: " + password)
+
+    path = os.getcwd() + '/' + "chromedriver"
+    driver = webdriver.Chrome(executable_path=path)
+    driver.get(BYS_LOGIN)
+    input()
 
 
 
